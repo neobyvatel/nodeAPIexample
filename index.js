0d8c5fe8ca92b564c483b71da3eaf5d17053997c
Adil <skeezygod@gmail.com>
2024-01-26 11:46:07 +0600

    fetch urls

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
-const express = require("express");
-const path = require("path");
+const weatherAPIkey = "e31236ca2959caf5178b8298a93073e8";
+const yandexMapAPIkey = "51c0bddc-7a68-44a3-9392-80d80e716549";
 
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+app.post("/weather", async (req, res) => {
+  const city = req.body.city;
 
+  let urls = [
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIkey}&units=metric`,
+    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIkey}&units=metric`,
+  ];
+
+  if (city.trim() === "") {
+    console.error("City name is empty");
+    return;
+  }
+  try {
+    let requests = urls.map((url) => fetch(url));
+
+    const responses = await Promise.all(requests);
+    const data = await Promise.all(
+      responses.map(async (response) => {
+        const jsonData = await response.json();
+        if (response.ok) {
+          return jsonData;
+        } else {
+          throw new Error(jsonData.message || "Error fetching data");
+        }
+      })
+    );
+
+    if (data[0] && data[1]) {
+      res.send({
+        weather: data[0],
+        forecast: data[1],
+      });
+    }
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
+    res.send({
+      error: error.message,
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
