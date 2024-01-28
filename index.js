22ce79ce58f74aa38e4e98f3cd57d9239df0c60d
Adil <skeezygod@gmail.com>
2024-01-28 09:08:35 +0600

    feat: yandexMaps api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const weatherAPIkey = "e31236ca2959caf5178b8298a93073e8";
-const yandexMapAPIkey = "51c0bddc-7a68-44a3-9392-80d80e716549";
+const yandexMapAPIkey = "5af0a515-c3c7-4c5e-9bb4-ddd4e32ed2d2";
 
 import express from "express";
 import path from "path";
@@ -7,6 +7,7 @@ import bodyParser from "body-parser";
 import fetch from "node-fetch";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+
 const app = express();
 const port = 3000;
 
@@ -17,49 +18,24 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
 app.post("/weather", async (req, res) => {
   const city = req.body.city;
-
-  let urls = [
+  const urls = [
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIkey}&units=metric`,
     `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIkey}&units=metric`,
   ];
-
-  if (city.trim() === "") {
-    console.error("City name is empty");
-    return;
-  }
-  try {
-    let requests = urls.map((url) => fetch(url));
-
-    const responses = await Promise.all(requests);
-    const data = await Promise.all(
-      responses.map(async (response) => {
-        const jsonData = await response.json();
-        if (response.ok) {
-          return jsonData;
-        } else {
-          throw new Error(jsonData.message || "Error fetching data");
-        }
-      })
-    );
-
-    if (data[0] && data[1]) {
-      res.send({
-        weather: data[0],
-        forecast: data[1],
-      });
-    }
-  } catch (error) {
-    console.error(`Error fetching data: ${error}`);
-    res.send({
-      error: error.message,
-    });
-  }
+  const [weatherResponse, forecastResponse] = await Promise.all(
+    urls.map((url) => fetch(url))
+  );
+  const weatherData = await weatherResponse.json();
+  const forecastData = await forecastResponse.json();
+  console.log(weatherData);
+  res.send({ weatherData, forecastData });
 });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+//
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
